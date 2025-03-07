882c44e9ddad5b3a366e27c4aa3f454691ab1665
akasky261 <gholapparth261@gmail.com>
2025-03-07 11:24:02 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,13 @@
 // App.js placeholder
+import React from 'react';
+import ScholarshipList from './components/ScholarshipList';
+
+function App() {
+  return (
+    <div className="App">
+      <ScholarshipList />
+    </div>
+  );
+}
+
+export default App;
